31f0e2ecaae58dfcfadefc548eab30c7ffa13090
analaurafra <analaura.francisco@gmail.com>
2024-10-27 21:07:03 -0300

    atualizando codigo de teste para get basico

--- equilibreapp/testes/teste_tarefas_get.py
+++ equilibreapp/testes/teste_tarefas_get.py
@@ -1,12 +1,6 @@
-import pytest
 from django.urls import reverse
-from pytest_django.asserts import assertContains
 
-@pytest.fixture
-def resposta(client):
-    resp = client.get(reverse('cadastro:home'))
-    return resp
 
-def test_status_code(resposta):
+def test_status_code(client):
+    resposta = client.get(reverse('cadastro:home'))
     assert resposta.status_code == 200
-
